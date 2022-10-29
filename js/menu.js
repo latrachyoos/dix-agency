74e5c4b5d7554202851bcdbf6c79ed741a068bc3
latrachyoos <91323411+latrachyoos@users.noreply.github.com>
2022-10-29 21:29:00 +0200

    v1

---
+++ js/menu.js
@@ -0,0 +1,10 @@
+function menu_func(){
+    let menu = document.getElementById("menu");
+    let open = document.getElementById("open");
+    let close = document.getElementById("close");
+   // menu.classList.toggle("nascondi");
+    menu.classList.toggle("mostra");
+    open.classList.toggle("hide");
+    close.classList.toggle("hide")
+
+}
